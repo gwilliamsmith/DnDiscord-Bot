91c613ec79cd1aca8874441996e6f415a68fc4e6
Will Smith <g.williamsmithiv@gmail.com>
2021-01-23 21:01:51 -0500

    Testing webhook on rpi

--- bot.js
+++ bot.js
@@ -14,6 +14,7 @@ client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+//Look through command folder for commands
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
