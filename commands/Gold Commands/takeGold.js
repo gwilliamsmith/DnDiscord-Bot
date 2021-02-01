f45b9fecd2e8f4a61260b2e53f688c0ac89afda9
Will Smith <g.williamsmithiv@gmail.com>
2021-01-31 21:12:23 -0500

    Add takeGold command
    Needs a coin-adjustment mode, but that's too complicated for now.
    
    Also added negative number validation to takeGold and giveGold.
    
    resolves #9

--- commands/Gold Commands/takeGold.js
+++ commands/Gold Commands/takeGold.js
@@ -4,8 +4,8 @@ const { prefix } = require('../../config.json')
 const goldSchema = require('../../schemas/goldSchema')
 
 module.exports = {
-    name: 'giveGold',
-    description: 'Gives gold to a player or party inventory',
+    name: 'takeGold',
+    description: 'Takes gold from a player or party inventory',
     minArgs: 1,
     maxArgs: 2,
     dbCommand: true,
@@ -14,21 +14,22 @@ module.exports = {
         run(message, args)
     },
     help: true,
-    helpString: `**${prefix}giveGold gives gold to a player or party inventory.
+    helpString: `**${prefix}takeGold takes gold from a player or party inventory.
 It takes a minimum of one, and a maximum of two arguments: \n
-[Wpp, Xgp, Ysp, Zcp]: The amount of coins to give. The values must be seperated by commas, and must include the denomination.
-(optional) [ <target> ]: The intended recipient of the gold. By default, the command gives gold to the person calling the command, though they can give gold to the party instead. The DM can give gold to anyone.`
+[Wpp, Xgp, Ysp, Zcp]: The amount of coins to take. The values must be seperated by commas, and must include the denomination.
+(optional) [ <target> ]: The person or party to take the gold from. By default, the command takes gold from the person calling the command, though they can take gold from the party instead. The DM can take gold from anyone.`
 }
 
 async function run(message, args){
     await mongo().then(async (mongoose) => {
-        try {
+        try{
             var target = message.author.id
             var name = message.author.username
             var p = 0
             var g = 0
             var s = 0
             var c = 0
+
             //If there are two args, they're trying to give gold to someone else.
             if(args.length == 2){
                 //If a user is mentioned, check if they're a DM. If not, complain
@@ -46,6 +47,13 @@ async function run(message, args){
                     target = 'party'
                     name = 'party'
                 }
+            }
+
+            //Check to see if the target has an entry in the database. If they don't, they've never been given gold and therefore have nothing to take
+            const check = await goldSchema.exists({owner : target, server_id : message.guild.id})
+            if(!check){
+                message.reply(` ${name} has no gold to take!`)
+                return
             }
 
             //Parse the platinum, gold, silver, and copper amounts into regular numbers
@@ -103,14 +111,46 @@ async function run(message, args){
             }
 
             const query = {owner : target, server_id : message.guild.id}
+
+            //Grab the coins the target
+            const targetData = await goldSchema.find(query)
+            const targetPlatinum = targetData[0]['platinum']
+            const targetGold = targetData[0]['gold']
+            const targetSilver = targetData[0]['silver']
+            const targetCopper = targetData[0]['copper']
+
+            //Default to strict mode - don't take more of any coin than the target has
+            //If a user wants to get change/exchange silver for gold they'll have to figure it out themselves
+            if(p > targetPlatinum && p > 0){
+                message.reply(` ${name} doesn't have enough platinum!`)
+                return
+            }
+            if(g > targetGold && g > 0){
+                message.reply(` ${name} doesn't have enough gold!`)
+                return
+            }
+            if(s > targetSilver && s > 0){
+                message.reply(` ${name} doesn't have enough silver!`)
+                return
+            }
+            if(c > targetCopper && c > 0){
+                message.reply(` ${name} doesn't have enough copper!`)
+                return
+            }
+
+            //Finally, update the record to adjust gold values
             await goldSchema.findOneAndUpdate(
                 query,{
-                    $inc: {platinum : +p, gold : +g, silver : +s, copper : +c}
+                    $inc : {
+                        platinum : -p,
+                        gold : -g,
+                        silver : -s,
+                        copper: -c
+                    }
                 },{
-                    upsert : true,
                     useFindAndModify : false
                 })
-            message.reply(` ${name} given ${args[0]}`)
+            message.reply(` took ${args[0]} from ${name}`)
         } finally {
             mongoose.connection.close()
         }
